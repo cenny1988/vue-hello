9684d4b3bb7a8bd7b33494d778fc339bfc8b1f5e
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-10 14:35:40 +0100

    create cartelle e file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1,
+utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Non c’è un layout prefissato.
+partite semplici,
+ma poi se volete rispolverate qualcosa di html-css per renderlo più carino.
+*/
+
