7f77c54927436875b87eafb490e0d50e394f0e93
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-10 14:44:04 +0100

    sezione bonus creata

---
+++ bonus/js/main.js
@@ -0,0 +1,17 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1,
+utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Non c’è un layout prefissato.
+partite semplici,
+ma poi se volete rispolverate qualcosa di html-css per renderlo più carino.
+*/
+
+var app = new Vue({
+    el: '#myapp',
+    data: {
+        message: 'Hello Vue'
+    }
+})
